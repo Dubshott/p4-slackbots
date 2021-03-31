bb4a0a28d51a2f3d1bf26568a87227c4ebcabfdc
Abhijay <fornitefrogs@gmail.com>
2021-03-31 12:54:59 -0700

    main.py

--- main.py
+++ main.py
@@ -28,12 +28,5 @@ def about():
 def game():
     return render_template("game.html")
 
-
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
-
-
 if __name__ == "__main__":
     app.run(debug=True, port="5001")
