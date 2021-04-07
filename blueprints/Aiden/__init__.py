acde30d7a5280eed055944bf6d03a6da47579cfb
largecodeman1 <73314755+largecodeman1@users.noreply.github.com>
2021-04-06 22:52:38 -0700

    Update __init__.py

--- blueprints/Aiden/__init__.py
+++ blueprints/Aiden/__init__.py
@@ -1,14 +1,10 @@
 from flask import Blueprint, render_template
 
-people_Aiden_bp = Blueprint('people_Aiden', __name__,
-                              template_folder='templates',
+people_aiden_bp = Blueprint('people_aiden', __name__,
+                              template_folder='aiden',
                               static_folder='static', static_url_path='assets')
 
 
-@people_Aiden_bp.route('/')
+@people_aiden_bp.route('/')
 def index():
-    return render_template("course/timelines.html", padlet="https://padlet.com/jmortensen7/cspcbproject")
-
-
-def blueprints_Aiden_bp():
-    return None
+    return render_template("randomcolorpicker.html")
