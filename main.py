e9f71cb39ff4742b7651743fd9616fc01c95c8c4
Abhijay <fornitefrogs@gmail.com>
2021-04-27 12:56:36 -0700

    Removed and Added Pokedex

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from flask import Flask, Blueprint, render_template
+"""
 from blueprints import blueprints_bp
 from blueprints.Abhijay import blueprints_Abhijay_bp
 from blueprints.Ak import blueprints_Ak_bp
 from blueprints.Aiden import blueprints_Aiden_bp
 from blueprints.Megan import blueprints_Megan_bp
 from blueprints.Zachary import blueprints_Zachary_bp
+"""
+"""
 
-
-app = Flask(__name__)
 app.register_blueprint(blueprints_bp, url_prefix='/blueprints/repos')
 app.register_blueprint(blueprints_Abhijay_bp, url_prefix='/blueprints/Abhijay')
 app.register_blueprint(blueprints_Ak_bp, url_prefix='/blueprints/Ak')
 app.register_blueprint(blueprints_Aiden_bp, url_prefix='/blueprints/Aiden')
 app.register_blueprint(blueprints_Megan_bp, url_prefix='/blueprints/Megan')
 app.register_blueprint(blueprints_Zachary_bp, url_prefix='/blueprints/Zachary')
-
+"""
+app = Flask(__name__)
 
 @app.route('/')
 def base():
@@ -27,6 +29,10 @@ def about():
 @app.route('/game')
 def game():
     return render_template("game.html")
+
+@app.route('/pokedex')
+def pokedex():
+    return render_template("pokedex.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port="5001")
--- server/core.js
+++
@@ -1,6 +0,0 @@
-
-var server = require("./libs/server");
-var color = require("./libs/color");
-
-
-server.init(3000, "Pokemon");
