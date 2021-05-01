a9112c9a6207087f2e28b30e6400ca8234f266fe
largecodeman1 <73314755+largecodeman1@users.noreply.github.com>
2021-05-01 00:31:20 -0700

    Update __init__.py

--- blueprints/Aiden/__init__.py
+++ blueprints/Aiden/__init__.py
@@ -4,6 +4,10 @@ people_Aiden_bp = Blueprint('people_Aiden', __name__,
                               template_folder='aiden',
                               static_folder='static', static_url_path='assets')
 
+@people_Aiden_bp.route('/bubblesort' , methods=['POST'])
+def bubbleroute():
+  if request.method == "POST"
+
 
 @people_Aiden_bp.route('/aiden/')
 def index():
