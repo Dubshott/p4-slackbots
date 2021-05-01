bf80d930618e9543ee42c0c3e9e8ec796b453e8e
zachajoseph <zachajoseph@gmail.com>
2021-05-01 16:17:47 -0700

    Views.py update

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from blueprints.Zachary.bubblesort import bubbleSort
 import projects #projects definitions are placed in different file
 from blueprints.Abhijay.__init__ import people_Abhijay_bp
 from blueprints.Aiden.__init__ import people_Aiden_bp
@@ -22,11 +23,38 @@ def base_route():
 def labstorage_route():
     return render_template("labstorage.html", projects=projects.setup())
 
-@zach_bp.route('/bubbleSort', methods=['GET', 'POST'])
-def bubbleSort
-    if request.method == 'POST':
-        try:
-            dataToSort = request.form["dataToSort"].lower()
+@app.route('/Mini-lab-storage-Zach')
+def zachlabstorage_route():
+    return render_template("Bubble_sort_zach.html", projects=projects.setup())
+
+@app.route('/bubbleSort',methods=["GET","POST"])
+def bubbleSort():
+    data = []
+    string = False
+    if request.form:
+       string = request.form.get("string")
+       data = string.split()
+       if request.form:
+            string = request.form.get("string")
+            data = string.split()
+            original_data = string.split()
+            if(request.form["select"] == "integer"):
+            # Need to convert all strings to numbers
+                try:
+                    for j in range (0,len(data)):
+                        data[j] = int(data[j])
+                    for j in range (0,len(original_data)):
+                        original_data[j] = int(original_data[j])
+                    return render_template("Bubble_sort_zach.html",output_list = bubbleSort(data,string).OuputList,original_list = original_data)
+                except ValueError:
+                    return render_template("Bubble_sort_zach.html",output_list = "Please enter Strings or Integers only",original_list = "Error")
+            else:
+                 try:
+                     string = True
+                     return render_template("Bubble_sort_zach.html",output_list = bubbleSort(data,string).OuputList,original_list = original_data)
+                 except ValueError:
+                       return render_template("Bubble_sort_zach.html",output_list = "Please enter Strings or Integers only",original_list = "Error")
+    return render_template("Bubble_sort_zach.html",output_list = bubbleSort(data,string).OutputList,original_list = data)
 
 
 if __name__ == "__main__":
