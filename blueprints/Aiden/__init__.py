8762dd59997ce993e34e7e6299c2b6892d617dee
largecodeman1 <73314755+largecodeman1@users.noreply.github.com>
2021-05-01 00:22:04 -0700

    Update __init__.py

--- blueprints/Aiden/__init__.py
+++ blueprints/Aiden/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
 people_Aiden_bp = Blueprint('people_Aiden', __name__,
                               template_folder='aiden',
