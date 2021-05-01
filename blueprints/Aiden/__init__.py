472ff5927dcbf096f8ff35b63e6a91ea3315aacd
largecodeman1 <73314755+largecodeman1@users.noreply.github.com>
2021-05-01 00:32:17 -0700

    Update __init__.py

--- blueprints/Aiden/__init__.py
+++ blueprints/Aiden/__init__.py
@@ -6,7 +6,7 @@ people_Aiden_bp = Blueprint('people_Aiden', __name__,
 
 @people_Aiden_bp.route('/bubblesort' , methods=['POST'])
 def bubbleroute():
-  if request.method == "POST"
+  if request.method == "POST":
 
 
 @people_Aiden_bp.route('/aiden/')
