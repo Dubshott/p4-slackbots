f1ed7775d457f306538eacad85fe4fd4db621ba3
zachajoseph <zachajoseph@gmail.com>
2021-05-01 16:17:11 -0700

    Bubblesort Update

--- blueprints/Zachary/bubblesort.py
+++ blueprints/Zachary/bubblesort.py
@@ -1,5 +1,5 @@
-class bubbleSort
-    def __init__(self, input_list, inputString)
+class bubbleSort:
+    def __init__(self, input_list,string):
 
         self.input_list = input_list
 
@@ -7,10 +7,10 @@ class bubbleSort
 
         self.bubbleSort(input_list)
 
-        self.inputString = inputString
+        self.string = string
 
-    def bubbleSort(self, arr):
-        n = len(arr)
+    def bubbleSort(self, data):
+        n = len(data)
 
         # Traverse through all array elements
         for i in range(n):
@@ -21,49 +21,43 @@ class bubbleSort
                 # traverse the array from 0 to n-i-1
                 # Swap if the element found is greater
                 # than the next element
-                if arr[j] > arr[j+1] :
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
-    def stringSort(self, arr):
+                if data[j] > data[j+1] :
+                    data[j], data[j+1] = data[j+1], arr[j]
+    def stringSort(self, data):
         alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'j', 'K', 'L',
                     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X',
                     'Y','Z']
-        for j in range(0, len(arr)):
-            for i in range(0, len(arr)):
+        for j in range(0, len(data)):
+            for i in range(0, len(data)):
                 _sorted = False
-                if i != len(arr)-1:
-                    for k in range(0,len(arr[i])):
-                if not _sorted:
-                    if k != (len(arr[i]) and len(arr[i+1])):
-                        if alphabet.index(arr[i][k]) > alphabet.index(arr[i+1][k]):
-                            arr[i], arr[i+1] = arr[i+1], arr[i]
-                            _sorted = True
-                        elif alphabet.index(arr[i][k]) < alphabet.index(arr[i+1][k]):
-                            _sorted = True
-                        else:
-                            if len(arr[i+1]) < len(arr[i]):
-                                arr[i], arr[i+1] = arr[i+1], arr[i]
-                                _sorted = True
-    def Stringconvert(self,arr):
-        for j in range(0, len(arr)):
-            arr[j] = arr[j].upper()
+                if i != len(data)-1:
+                    for k in range(0,len(data[i])):
+                        if not _sorted:
+                            if k != (len(data[i]) and len(data[i+1])):
+                                if alphabet.index(data[i][k]) > alphabet.index(data[i+1][k]):
+                                    data[i], data[i+1] = data[i+1], data[i]
+                                    _sorted = True
+                                elif alphabet.index(data[i][k]) < alphabet.index(data[i+1][k]):
+                                    _sorted = True
+                                else:
+                                    if len(data[i+1]) < len(data[i]):
+                                        data[i], data[i+1] = data[i+1], data[i]
+                                        _sorted = True
+    def Stringconvert(self ,data):
+        for j in range(0, len(data)):
+            data[j] = data[j].upper()
 
     @property
     def OuputList(self):
-        arr = self.input_list
-        if(self.isString):
-            self.Stringconvert(arr)
-            self.stringSort(arr)
-            for i in range(len(arr)):
-                self._output_list.append(arr[i])
+        data = self.input_list
+        if(self.String):
+            self.Stringconvert(data)
+            self.stringSort(data)
+            for i in range(len(data)):
+                self._output_list.append(data[i])
             return self._output_list
         else:
-            self.bubbleSort(arr)
-            for i in range(len(arr)):
-                self._output_list.append(arr[i])
+            self.bubbleSort(data)
+            for i in range(len(data)):
+                self._output_list.append(data[i])
             return self._output_list
-
-# Driver code to test above
-if __name__ == "__main__":
-    arr = [1, 3, 7, 9]
-    bubble = bubbleSort(arr,False)
-    print(bubble.OuputList)
