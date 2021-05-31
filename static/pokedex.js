02d54554c5611531fda30d83fe2ec29035e414ce
Dubshott <55414361+Dubshott@users.noreply.github.com>
2021-05-31 11:07:17 -0700

    Finished commenting JS

--- static/pokedex.js
+++ static/pokedex.js
@@ -27,11 +27,13 @@ const fetchPokemon = async () => {
     displayPokemon(pokemon);
 };
 
+// This section of code is just taking our pokemon data and converting it into an HTML format
 const displayPokemon = (pokemon) => {
     const pokemonHTMLString = pokemon
         .map(
             (pokeman) =>
                 `
+    <!-- This is just basic HTMl now and formatting all the data we recieved onto the webpage -->
     <li class="card" onclick="selectPokemon(${pokeman.id})">
         <img class="card-image" src="${pokeman.image}"/>
         <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
@@ -43,8 +45,10 @@ const displayPokemon = (pokemon) => {
     pokedex.innerHTML = pokemonHTMLString;
 };
 
+// What this section does is when you press the pokemon's picture, it will show you even more data on the pokemon
 const selectPokemon = async (id) => {
     if (!cachedPokemon[id]) {
+        // Once again drawing data from the pokeapi site and following the same steps as above to extract the data
         const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
         const res = await fetch(url);
         const pokeman = await res.json();
@@ -53,11 +57,15 @@ const selectPokemon = async (id) => {
     } else {
         displayPokemanPopup(cachedPokemon[id]);
     }
+    // This just allows the data to be brought in from pokeapi again including weight, height, and those kind of things
 };
 
+// Now this section of code actually converts that data and formats it into our HTML so we can actually see the data
 const displayPokemanPopup = (pokeman) => {
     console.log(pokeman);
     const type = pokeman.types.map((type) => type.type.name).join(', ');
+    // This point converts it to the HTML and allows us to see a close button at the top right of the screen as well
+    // Also allows us the pokemon and things in the middle of the screen
     const htmlString = `
         <div class="popup">
             <button id="closeBtn" onclick="closePopup()">Close</button>
@@ -65,6 +73,7 @@ const displayPokemanPopup = (pokeman) => {
                 <img class="card-image" src="${
         pokeman.sprites['front_default']
     }"/>
+                <!-- This section of code formats the data received and shows the name, type, height, and weight -->
                 <h2 class="card-title">${pokeman.name}</h2>
                 <p><small>Type: ${type} | Height:</small> ${pokeman.height} | Weight: ${
         pokeman.weight
@@ -75,9 +84,11 @@ const displayPokemanPopup = (pokeman) => {
     pokedex.innerHTML = htmlString + pokedex.innerHTML;
 };
 
+// When the user presses 'close', the website will bring it back to the original pokedex
 const closePopup = () => {
     const popup = document.querySelector('.popup');
     popup.parentElement.removeChild(popup);
 };
 
+// Calls the initial function which will go through this entire process
 fetchPokemon();
