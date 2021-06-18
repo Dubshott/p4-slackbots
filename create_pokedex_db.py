4d592090b1d3d694c914d81ddc66fbddff7031ce
zachajoseph <zachajoseph@gmail.com>
2021-06-18 01:33:12 -0700

    Search bar added

--- create_pokedex_db.py
+++ create_pokedex_db.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, inspect
 import pprint
@@ -21,6 +21,36 @@ class Pokemon(db.Model):
     height = db.Column(db.String(255), nullable=False)
     weight = db.Column(db.Integer, nullable=False)
     image = db.Column(db.Text, nullable=False)
+
+class PokemonTypeRating(db.Model):
+    PokemonType_name = db.Column(db.String(255), primary_key=True, nullable=False)
+    like_count = db.Column(db.integer, nullable=False)
+    dislike_count = db.Column(db.integer, nullable=False)
+
+def addPokemonTypeRating(PokemonType):
+    pokemonType_rec = PokemonTypeRating.query.fiter_by(PokemonType_name=PokemonType).first()
+    if pokemonType_rec and pokemonType.PokemonType_name == PokemonType:
+        return
+    else:
+        new_pokemonType_rec = PokemonTypeRating(PokemonType_name=PokemonType, like_count=0, dislike_count=0)
+        db.session.add(new_pokemonType_rec)
+        db.session.commit()
+
+def findPokemonTypeRating(PokemonType):
+    pokemonType_rec = PokemonTypeRating.query.fiter_by(PokemonType_name=PokemonType).first()
+    if pokemonType_rec and pokemonType_rec.PokemonType_name == PokemonType:
+        return pokemonType_rec
+    return
+
+
+def update_like_count(pokemonType_rec):
+    pokemonType_rec.like_count++
+    db.session.commit()
+
+def update_dislike_count(pokemonType_rec):
+    pokemonType_rec.dislike_count++
+    db.session.commit()
+
 
 def addPokemon(Name, Type, Height, Weight, Image):
     pokemon = Pokemon.query.filter_by(name=Name).first()
@@ -55,12 +85,18 @@ def getPokemon(Name):
         Height = pokemon.height
         Weight = pokemon.weight
         Image = pokemon.image
-    response = jsonify({'name':Name, 'type': Type, 'height':Height, 'weight':Weight, 'image':Image})
-    return response
+    resp = {'name':Name, 'type': Type, 'height':Height, 'weight':Weight, 'image':Image}
+    print(resp)
+    return resp
 
 
 ''' table creation '''
 db.create_all()
+
+''' Enter Data in PokemonTypeRating Table'''
+for type in ['Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic',
+             'Bug', 'Rock', 'Ghost', 'Dark', 'Dragon', 'Steel', 'Fairy']:
+    addPokemonTypeRating(type)
 
 ''' inspect table '''
 engine = create_engine(dbURI)
